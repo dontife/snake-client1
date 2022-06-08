eba1e5b6870fcb159b3df4e9bd36afca91fcd5b8
Boluwatife Ade Ogunlola <dontife@ymail.com>
2022-06-08 20:27:32 +0000

    refactored the code by creating a client file tohandle the connection and exporting the function to the play.js file

---
+++ client.js
@@ -0,0 +1,23 @@
+// net module provides an asynchronous network API for creating stream-based tcp servers
+const net = require('net');
+
+// establishes a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: '50541'
+  });
+  conn.setEncoding('utf8');
+
+  conn.on('connect', () => {
+    //code that does something when the connection is first established
+    console.log('connected to server');
+  });
+  // implementing data handler to see message sent from the server
+  conn.on('data', (data) => {
+    console.log("Server says: ", data);
+  });
+  return conn;
+};
+
+module.exports = {connect, };
