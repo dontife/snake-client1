3cce8bb01801c31220b6bd317f084bf48f97f615
Boluwatife Ade Ogunlola <dontife@ymail.com>
2022-06-08 23:19:44 +0000

    binding the movement commands to the w a s d keys also modifying the setInput function by adding a connection object argument

--- input.js
+++ input.js
@@ -1,5 +1,8 @@
+
+let connection;
 // setting up stdin
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -12,6 +15,19 @@ const setupInput = function () {
 
 // specifies what happnes when data is received from stdin
 const handleUserInput = function (key) {
+  // binding the movement commands to the w a s d  keys
+  if (key === 'w') {
+    connection.write('Move: up');
+  }
+  if (key === 'a') {
+    connection.write('Move: left');
+  }
+  if (key === 's') {
+    connection.write('Move: down');
+  }
+  if (key === 'd') {
+    connection.write('Move: right');
+  }
   // if ctr1 + c is inputted, the game gets terminated
   if (key === '\u0003') {
     process.exit();
